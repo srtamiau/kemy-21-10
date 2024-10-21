3e1b66db9f0a9c15169cfae5f0848383f9455884
srtamiau <165675518+srtamiau@users.noreply.github.com>
2024-10-21 11:22:28 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Bíblico',
+    'Qual o nome do homem mais velho da Bília?',
+    'Matusalém, com 969 anos'
+)
+
+criaCartao(
+    'Bíblico',
+    'Quantas praga foram enviadas ao Egito?',
+    'foram enviadas no total de 10 pragas'
+)
+
+criaCartao(
+    'Bíblico',
+    'Quantos apóstolos Jesus tinha?',
+    'Jesu tinha 12 apóstolos'
+)
+
+criaCartao(
+    'Bíblico',
+    'Quem era o "gentios" da Bíblia?',
+    'Eram os "não judeus"'
+)
